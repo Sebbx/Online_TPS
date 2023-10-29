d8d74e94abdcac45e7b9f2d662bfaad731fe5839
Sebastian <s.rubacha@actum.games>
2023-10-29 20:08:31 +0100

    Enchanced input setup

--- Source/OnlineTPS/TPSCharacter.cpp
+++ Source/OnlineTPS/TPSCharacter.cpp
@@ -2,6 +2,9 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 ATPSCharacter::ATPSCharacter()
 {
@@ -15,12 +18,59 @@ ATPSCharacter::ATPSCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
+
+	
 }
 
 void ATPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+}
+
+void ATPSCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FVector Forward = GetActorForwardVector();
+	AddMovementInput(Forward, MovementVector.Y);
+
+	const FVector Right = GetActorRightVector();
+	AddMovementInput(Right, MovementVector.X);
+
+	/*More complex movement method
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
 	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightDirection, MovementVector.X);
+	*/
+}
+
+void ATPSCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector =  Value.Get<FVector2D>();
+
+	AddControllerPitchInput(LookAxisVector.Y);
+	AddControllerYawInput(LookAxisVector.X);
+}
+
+void ATPSCharacter::Jump(const FInputActionValue& Value)
+{
+	const bool CurrentValue = Value.Get<bool>();
+	if (CurrentValue)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Purple, TEXT("IA_Jump triggered"));
+	}
 }
 
 void ATPSCharacter::Tick(float DeltaTime)
@@ -33,5 +83,11 @@ void ATPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATPSCharacter::Move);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ATPSCharacter::Jump);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ATPSCharacter::Look);
+	}
 }
 
--- Source/OnlineTPS/TPSCharacter.h
+++ Source/OnlineTPS/TPSCharacter.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "TPSCharacter.generated.h"
 
@@ -14,9 +15,30 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Jump(const FInputActionValue& Value);
+	
+	
 protected:
 	virtual void BeginPlay() override;
 
+	
+	// Input
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enchanced Input")
+	class UInputMappingContext* InputMappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enchanced Input")
+	class UInputAction* JumpAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enchanced Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enchanced Input")
+	UInputAction* LookAction;
+
+
+	
 private:
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	class USpringArmComponent* CameraBoom;
@@ -24,6 +46,7 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	class UCameraComponent* FollowCamera;
 
+	
 	//Space for getters and setter
 public:
 
