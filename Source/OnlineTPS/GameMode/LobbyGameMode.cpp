fcad883c5b74c6d86ed5fa76082a4bde3cb0188e
Sebastian <s.rubacha@actum.games>
2023-10-30 08:55:35 +0100

    hotfix for ShooterMap ServerTravel

--- Source/OnlineTPS/GameMode/LobbyGameMode.cpp
+++ Source/OnlineTPS/GameMode/LobbyGameMode.cpp
@@ -9,9 +9,13 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 
+	GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Purple, TEXT("Post Login"));
+
 	int32 NumberOfPlayers =  GameState.Get()->PlayerArray.Num();
 	if (NumberOfPlayers == 2)
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, TEXT("Post Login, players == 2"));
+	
 		UWorld* World = GetWorld();
 		if (World)
 		{
